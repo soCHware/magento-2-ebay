c336f5f638bed04fb9d3d312b54869ad7460fd35
ch1986 <ccgchristian@gmcil.com>
2016-03-26 19:40:18 +0100

    Second commit

---
+++ registration.php
@@ -0,0 +1,5 @@
+<?php
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(\Magento\Framework\Component\ComponentRegistrar::MODULE, 'soCHware_Magento2Ebay', __DIR__);
